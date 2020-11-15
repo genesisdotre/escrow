c37c467f25a0c5319ea5e49536845ec31208c137
Mars <email@genesis.re>
2020-11-15 18:28:35 +0000

    Sending build files!

---
+++ build/precache-manifest.d4199d6d05350a5a03939511b2054867.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = [
+  {
+    "revision": "93978346d34e576287053c338a61154e",
+    "url": "/static/media/dispute.93978346.svg"
+  },
+  {
+    "revision": "88ff19a2aba4dd1ba26f",
+    "url": "/static/css/main.1cfb3e47.chunk.css"
+  },
+  {
+    "revision": "c1a7f243c25f39b25314",
+    "url": "/static/js/1.c1a7f243.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c7cae3ba0089608f8c077866b583a42a",
+    "url": "/static/media/warning.c7cae3ba.png"
+  },
+  {
+    "revision": "9109a8dd2d56fa249c99cd01991d6dd8",
+    "url": "/static/media/logo-updated.9109a8dd.png"
+  },
+  {
+    "revision": "33901ab60517c3cd3111b9f5d0c33da2",
+    "url": "/static/media/eth.33901ab6.png"
+  },
+  {
+    "revision": "11446df33e44e60fb47bbc6810bb2dbd",
+    "url": "/static/media/meta-mask-logo.11446df3.png"
+  },
+  {
+    "revision": "4abf8c6af719f6144316752e2dbc1c7b",
+    "url": "/static/media/BBB.4abf8c6a.png"
+  },
+  {
+    "revision": "fce507c91f0463aff4311457c0e7f356",
+    "url": "/static/media/crypto-transaction.fce507c9.png"
+  },
+  {
+    "revision": "6871142dd7f5ece6a2e53850111bef93",
+    "url": "/static/media/crypto-transaction-white.6871142d.png"
+  },
+  {
+    "revision": "a8d28941ba5d445d199677b1fc187cdf",
+    "url": "/static/media/general-service.a8d28941.png"
+  },
+  {
+    "revision": "627d192e9b38d5677c9298eea69a09fa",
+    "url": "/static/media/general-service-white.627d192e.png"
+  },
+  {
+    "revision": "fb462e0e0072b818bd05876e8034f314",
+    "url": "/static/media/icon-success-blue.fb462e0e.svg"
+  },
+  {
+    "revision": "7a1780cefff967aaf2fad9e871da29e0",
+    "url": "/static/media/make-payment.7a1780ce.svg"
+  },
+  {
+    "revision": "8ace28596e7f1fc029d62235c527447d",
+    "url": "/static/media/time.8ace2859.svg"
+  },
+  {
+    "revision": "b78395d2c77d770939594ad2ea9d0105",
+    "url": "/static/media/settlement-in-progress.b78395d2.svg"
+  },
+  {
+    "revision": "b306ad11c133b122f911fe1f53da81c3",
+    "url": "/static/media/icon-dispute-red.b306ad11.svg"
+  },
+  {
+    "revision": "88ff19a2aba4dd1ba26f",
+    "url": "/static/js/main.88ff19a2.chunk.js"
+  },
+  {
+    "revision": "f99c059dff8b6e7a89f75e4cf0831da0",
+    "url": "/static/media/success.f99c059d.svg"
+  },
+  {
+    "revision": "76362dcd2129b0c7bf95a561698d48b3",
+    "url": "/static/media/ellipsis.76362dcd.svg"
+  },
+  {
+    "revision": "a976fa2297271452144e9c6e78c801a7",
+    "url": "/static/media/primary-document.a976fa22.svg"
+  },
+  {
+    "revision": "b2a7adb9da22911ae18089be28b31c1f",
+    "url": "/static/media/image.b2a7adb9.svg"
+  },
+  {
+    "revision": "756e122bd93883b2c70b60d0fec39a11",
+    "url": "/static/media/link.756e122b.svg"
+  },
+  {
+    "revision": "2f290704cc8f3c2d441027f6076acd7e",
+    "url": "/static/media/video.2f290704.svg"
+  },
+  {
+    "revision": "fe584d6b2b5c7b181d3c2683b3e1524a",
+    "url": "/static/media/question-circle.fe584d6b.svg"
+  },
+  {
+    "revision": "abf6a52badfbf345d1ddcf7f149d9b82",
+    "url": "/static/media/twitter.abf6a52b.svg"
+  },
+  {
+    "revision": "0776297f1840cb9613cddbcbf4a3e247",
+    "url": "/static/media/github.0776297f.svg"
+  },
+  {
+    "revision": "d91cca5205cb087ad09a84ee76a47754",
+    "url": "/static/media/ghost.d91cca52.svg"
+  },
+  {
+    "revision": "3318b70c4bcdc53766c537ca7372ca54",
+    "url": "/static/media/linkedin.3318b70c.svg"
+  },
+  {
+    "revision": "703cf2019aca6df64bc620fd57d04b0a",
+    "url": "/static/media/telegram.703cf201.svg"
+  },
+  {
+    "revision": "942807e277e5fc2393bb4ae80ac92eb6",
+    "url": "/static/media/kleros.942807e2.svg"
+  },
+  {
+    "revision": "ab44e50a8e90d7d009f1ab387c3ae630",
+    "url": "/static/media/transaction.ab44e50a.svg"
+  },
+  {
+    "revision": "90ef17df88bc71999b1cc2fc4edbaa5c",
+    "url": "/static/media/envelope.90ef17df.svg"
+  },
+  {
+    "revision": "ec4f1efcf41605c72a45814c7bfb3ae5",
+    "url": "/static/media/invoice.ec4f1efc.svg"
+  },
+  {
+    "revision": "5e8fc02817b137acd92aa07b1416eb74",
+    "url": "/static/media/worldwide.5e8fc028.svg"
+  },
+  {
+    "revision": "61ed8919af07e326396af1caa3d53856",
+    "url": "/index.html"
+  }
+];
